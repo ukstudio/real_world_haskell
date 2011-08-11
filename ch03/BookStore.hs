6991d61d194b4a88dace32c6e3ebd3988b80d607
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-08-11 11:58:15 +0900

    3.3 代数データ型
    - 各値構成子はゼロ個以上の引数を取れる

--- ch03/BookStore.hs
+++ ch03/BookStore.hs
@@ -15,3 +15,12 @@ type ReviewBody = String
 data BetterReview = BetterReview BookInfo CustomerID ReviewBody
 
 type BookRecord = (BookInfo, BookReview)
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
