bcbb4085ce2db57e6d6930ad22947d131f08541d
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-09-28 05:12:10 +0900

    3.10 式のオフサイドルールおよび白空白

---
+++ ch03/BadIndent.hs
@@ -0,0 +1,7 @@
+-- ここが一番左のカラムです
+
+    -- 最初の宣言はカラム4から始まっています
+    firstBadIndentation = 1
+
+  -- 2番目の宣言は最初のものより左にあり、これは不正です
+  secondBadIndentation = 2
---
+++ ch03/GoodIndent.hs
@@ -0,0 +1,7 @@
+-- ここが一番左のカラムです
+
+    -- トップレベルの宣言は任意のカラム
+    firstGoodIndentation = 1
+
+    -- それ以降すべての宣言で同じにすれば上手くいきます
+    secondGoodIndentation = 2
